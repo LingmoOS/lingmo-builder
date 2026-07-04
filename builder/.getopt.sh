242e30cc7ba5a5a35705f3e47f3a5c422612a4b6
Lingmo <lingmo@lingmo.org>
2026-07-04 17:40:14 +0800

    重构构建脚本，优化lmforge克隆步骤，添加构建版本生成函数，更新文件权限

---
+++ builder/.getopt.sh
@@ -0,0 +1,2 @@
+BUILD_OPTS_SHORT="d:pa:vD:s:h"
+BUILD_OPTS_LONG="distribution:,proposed-updates,arch:,verbose,debug,salt:,help,installer,live,variant:,version:,subdir:,get-image-path,clean,no-clean"
